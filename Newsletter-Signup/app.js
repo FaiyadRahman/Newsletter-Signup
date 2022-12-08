4d63e0f297fb45cf63052d009706940ec9e1fc7c
FaiyadRahman <faiyad.r@yahoo.com>
2022-12-08 00:31:38 -0700

    upload files

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+require('dotenv').config();
+
+var API_KEY = process.env.API_KEY;
+var LIST_ID = process.env.LIST_ID;
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(
+    "first: " + firstName + "\nlast: " + lastName + "\nemail: " + email
+  );
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+  var url = "https://us14.api.mailchimp.com/3.0/lists/" + LIST_ID;
+  var options = {
+    method: "POST",
+    auth: "anystring:" + API_KEY,
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
