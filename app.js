e00f38c579b5eeee341b525a39ee16dac87d529b
FaiyadRahman <faiyad.r@yahoo.com>
2022-12-08 01:33:10 -0700

    add gitignore

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   // console.log("listening on port 3000");
   console.log("apikey: " + API_KEY);
   console.log("list ID: " + LIST_ID);
